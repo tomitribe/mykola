9ddd9fe525629011dc5c243871e0bb0a03b9fd8b
Dexmaster <dexteritymaster@gmail.com>
2016-11-10 12:49:16 +0200

    Initial commit

---
+++ components/angular-tomitribe-diff/webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: __dirname + "/src",
+  entry:  {
+    component: './tomitribe-diff.ts',
+  },
+  output: {
+    path: __dirname +"/dist",
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader']
+      },
+      {
+        test: /\.jade$/,
+        loader: 'pug-html-loader'
+      },
+      {
+        test: /\.sass$/,
+        loader: "style-loader!css-loader!postcss-loader!sass-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+
+  postcss: function () {
+    return [precss, autoprefixer];
+  },
+
+  tslint: {
+    emitErrors: false,
+    failOnHint: true
+  },
+
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './tomitribe-diff.jade'
+    })
+  ],
+
+  htmlLoader: {
+    minimize: false
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+}
