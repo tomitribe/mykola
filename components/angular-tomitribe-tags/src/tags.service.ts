a443002c7a8172ba4839033c2a44855ab5c3b366
jgomes <joaopggomes@gmail.com>
2017-04-03 15:10:24 +0100

    TAG-1563

--- components/angular-tomitribe-tags/src/tags.service.ts
+++ components/angular-tomitribe-tags/src/tags.service.ts
@@ -1,8 +1,9 @@
 export class TagReference {
-  constructor(public id: string,
-              public name: string,
-              public description: string/*future: tooltip*/) {
-  }
+    constructor(public id: string,
+                public name: string,
+                public attributes: {}) {
+
+    }
 }
 
 export class TagsService {
@@ -11,16 +12,14 @@ export class TagsService {
     constructor(private $http, private config) {
     }
 
-    findTags() {
-        return this.$http.get(this.config.tagsEndpoint)
-          .then(d => d.data)
-          .then(d => d.items)
-          .then(d => (d || []).map(i => { // tolerate {id, name, description} and {name, displayName, description} as model
-            return {
-              name: i.id || i.name,
-              displayName: i.displayName || i.name,
-              description: i.description
-            };
-          }));
+    findTags(query) {
+        let params = {};
+        params['field'] = "name";
+
+        if (query && query.length) {
+            params['query'] = query;
+        }
+
+        return this.$http.get(this.config.tagsEndpoint, {params: params}).then(d => d.data && d.data.items ? d.data.items : []);
     }
 }
